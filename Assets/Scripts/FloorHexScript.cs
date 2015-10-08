30e0fa5651331b3eab8626fb5a58c6dcaf280a26
Nigh7Sh4de <plotnikd@gmail.com>
2015-10-08 19:20:30 -0400

    Did a bunch of stuff

---
+++ Assets/Scripts/FloorHexScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorHexScript : MonoBehaviour {
+
+    public Transform FloorPiece;
+
+	// Use this for initialization
+	void Start () {
+        var pos = gameObject.transform.position;
+        Instantiate(FloorPiece, RelativeVector(pos, 0, 0, 0), Quaternion.Euler(0, 0, 0));
+        Instantiate(FloorPiece, RelativeVector(pos, -0.43f, 0, 0.25f), Quaternion.Euler(0, 60, 0));
+        Instantiate(FloorPiece, RelativeVector(pos, -0.43f, 0, 0.75f), Quaternion.Euler(0, 120, 0));
+        Instantiate(FloorPiece, RelativeVector(pos, 0, 0, 1), Quaternion.Euler(0, 180, 0));
+        Instantiate(FloorPiece, RelativeVector(pos, 0.43f, 0, 0.75f), Quaternion.Euler(0, 240, 0));
+        Instantiate(FloorPiece, RelativeVector(pos, 0.43f, 0, 0.25f), Quaternion.Euler(0, 300, 0));
+	}
+
+    private Vector3 RelativeVector(Vector3 pos, float x, float y, float z)
+    {
+        return new Vector3(pos.x + x, pos.y + y, pos.z + z);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+    private Rigidbody rb;
+    private float MUL = 500;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.W))
+        {
+            var v3 = Vector3.one;
+            v3.Scale(new Vector3(0, 0, MUL));
+            rb.AddRelativeForce(v3);
+        }
+	}
+}
