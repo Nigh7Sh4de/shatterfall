3d14a3723cc250ce9c9464e0494f24fe2b39d9b6
nigh7sh4de <plotnikd@gmail.com>
2016-01-09 18:02:04 -0500

    Moved floor drop mechanic to floor script

---
+++ shatterfall/Assets/Scripts/floor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class floor : MonoBehaviour {
+
+    Rigidbody rigidbody;
+
+    public void Drop()
+    {
+        rigidbody.isKinematic = false;
+    }
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- shatterfall/Assets/Scripts/orb.cs
+++ shatterfall/Assets/Scripts/orb.cs
@@ -6,20 +6,46 @@ public class orb : MonoBehaviour {
     Rigidbody rigidbody;
 
     const int SPEED = 500;
+    ArrayList collisions;
 
     // Use this for initialization
     void Start () {
         rigidbody = GetComponent<Rigidbody>();
+        collisions = new ArrayList();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        collisions.Add(other.GetComponent<floor>());
+
+        //Debug.Log("orb->" + other.name + " " + collisions.Count);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        collisions.Remove(other.GetComponent<floor>());
+
+        Debug.Log("orb->" + other.name + " " + collisions.Count);
+
 
     }
 
     public void Explode()
     {
+
+        foreach (floor other in collisions)
+        {
+            if (other != null)
+                other.Drop();
+        }
+
+
         gameObject.SetActive(false);
     }
 
     public void Activate(Vector3 position, Quaternion rotation)
     {
+        collisions = new ArrayList();
         transform.position = position;
         transform.rotation = rotation;
         gameObject.SetActive(true);
