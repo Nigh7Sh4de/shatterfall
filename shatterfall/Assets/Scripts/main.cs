0b73605d1afd9899cfefa787521b1352a41f8545
nigh7sh4de <plotnikd@gmail.com>
2016-01-09 19:10:17 -0500

    Fix floor piece tesselation

--- shatterfall/Assets/Scripts/main.cs
+++ shatterfall/Assets/Scripts/main.cs
@@ -45,7 +45,7 @@ public class main : MonoBehaviour
             for (int i = 0; i < MAX - j; i++)
             {
                 GameObject.Instantiate(triangle, new Vector3(0.375f * j + 0.866f * i, 0, 0.75f * j), Quaternion.Euler(new Vector3(-90, 0, 0)));
-                GameObject.Instantiate(triangle, new Vector3(0.433f + 0.375f * j + 0.866f * i, 0, 0.25f + 0.75f * j), Quaternion.Euler(new Vector3(90, 0, 0)));
+                GameObject.Instantiate(triangle, new Vector3(0.433f + 0.375f * j + 0.866f * i, 0, 0.25f + 0.75f * j), Quaternion.Euler(new Vector3(-90, 180, 0)));
             }
 
     }
