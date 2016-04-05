1bc35fe95d939b16d3ecc67b05c2da5b70720299
Nigh7Sh4de <plotnikd@gmail.com>
2016-04-05 12:25:13 -0400

    Remove multi-press of A on win menu

--- shatterfall/Assets/Scripts/selector2.cs
+++ shatterfall/Assets/Scripts/selector2.cs
@@ -20,6 +20,7 @@ public class selector2 : MonoBehaviour {
 		option = 0;
 		transform.localScale = uiChoices [0].transform.localScale * 9f;
 		selectable = true;
+        ready = 0;
 	}
 	
 	float ready;
@@ -67,7 +68,8 @@ public class selector2 : MonoBehaviour {
 
         transform.position = uiChoices[option].transform.position + new Vector3(-uiChoices[option].transform.localScale.x * 0.55f, uiChoices[option].transform.localScale.x * 0.2f, 0);
 		
-		if ((Input.GetKeyDown (KeyCode.Space) || Input.GetAxis("Jump1") > 0)) {
+		if ((Input.GetKeyDown (KeyCode.Space) || Input.GetAxis("Jump1") > 0) && ready <= 0) {
+            ready = READY_DELAY;
 			if(option == 0 && selectable){
 				source.PlayOneShot (selectSound, 1F);
 				StartCoroutine(loadMainScene());
